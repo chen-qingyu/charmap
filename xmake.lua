9f0004a42604d66b805f3995d894586ddf5ba523
chen-qingyu <chen_qingyu@qq.com>
2025-02-03 09:01:46 +0800

    pack readme

--- xmake.lua
+++ xmake.lua
@@ -19,6 +19,7 @@ xpack("greekchar")
     set_homepage("https://github.com/chen-qingyu/greek-char")
     set_bindir("/")
     add_targets("greekchar")
+    add_installfiles("README.md")
     local qt_path = "C:/Qt/6.8.1/msvc2022_64"
     add_installfiles(qt_path .. "/bin/Qt6Core.dll", qt_path .. "/bin/Qt6Gui.dll",  qt_path .. "/bin/Qt6Widgets.dll")
     add_installfiles(qt_path .. "/plugins/platforms/qwindows.dll", {prefixdir = "platforms/"})
